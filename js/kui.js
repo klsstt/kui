708b744a9754d74873d0e4689dd2d565781c0a86
klsstt <klsstt@qq.com>
2018-06-12 19:46:29 +0800

    combit

--- js/kui.js
+++ js/kui.js
@@ -6408,6 +6408,67 @@ function($) {
         }
     };
 }(jQuery);
+
+(function(document, $) {
+    'use strict';
+    $["kui"]["hover"] = {
+        '$instance': $(".kui-hover"),
+        'storageKey': "kui.hover",
+        "init": function() {
+            var _this = $(this),
+                svg = "<svg width=\"0\" height=\"0\"><defs><filter id=\"goo\"><feGaussianBlur in=\"SourceGraphic\" stdDeviation=\"10\" result=\"blur\"></feGaussianBlur>" +
+                " <feColorMatrix in=\"blur\" mode=\"matrix\" values=\"1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9\" result=\"goo\"></feColorMatrix>" +
+                " <feComposite in=\"SourceGraphic\" in2=\"goo\" operator=\"atop\"></feComposite></filter></defs></svg>";
+            _this[0].$instance.append(svg);
+            this['bind']();
+        },
+        'bind': function() {
+            var _this = $(this),
+                hover = _this[0].$instance["find"](".kui-hover-nav"),
+                ua = navigator.userAgent;
+            /Safari|iPhone/i.test(ua) && 0 == /chrome/i.test(ua) && hover.addClass("no-filter");
+            var drags = {
+                    down: !1,
+                    x: 0,
+                    y: 0,
+                    winWid: 0,
+                    winHei: 0,
+                    clientX: 0,
+                    clientY: 0
+                },
+                asideNav = hover[0],
+                getCss = function(a, e) {
+                    return a.currentStyle ? a.currentStyle[e] : document.defaultView.getComputedStyle(a, !1)[e]
+                };
+            hover.on("mousedown", function(a) {
+                drags.down = !0,
+                    drags.clientX = a.clientX,
+                    drags.clientY = a.clientY,
+                    drags.x = getCss(this, "right"),
+                    drags.y = getCss(this, "top"),
+                    drags.winHei = $(window).height(),
+                    drags.winWid = $(window).width(),
+                    $(document).on("mousemove", function(a) {
+                        if (drags.winWid > 640 && (a.clientX < 120 || a.clientX > drags.winWid - 50)) //50px
+                            return !1 /*,console.log(!1)*/ ;
+                        if (a.clientY < 180 || a.clientY > drags.winHei - 120) //导航高度
+                            return !1;
+                        var e = a.clientX - drags.clientX,
+                            t = a.clientY - drags.clientY;
+                        asideNav.style.top = parseInt(drags.y) + t + "px";
+                        asideNav.style.right = parseInt(drags.x) - e + "px";
+                    })
+            }).on("mouseup", function() {
+                drags.down = !1, $(document).off("mousemove")
+            });
+        }
+    };
+    //run kuihover
+    $(function() {
+        $["kui"]["hover"].init();
+    });
+
+})(document, jQuery);
 /**
  * 工具类
  */
@@ -6420,5 +6481,5 @@ function($) {
             if (r != null) return unescape(r[2]);
             return null;
         }
-    }
+    };
 })(jQuery);
