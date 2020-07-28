cede7b1c820e4e8e042b049bf4a66c4e2a3e2249
klsstt@qq.com <klsstt@qq.com>
2020-07-28 14:12:13 +0800

    首页开发第一版

--- gulpfile.js
+++ gulpfile.js
@@ -6,53 +6,62 @@ const uglify = require('gulp-uglify') // js压缩
 const sass = require('gulp-sass')// sass编译
 var css = require('gulp-clean-css');
 const concat = require('gulp-concat') // 代码合并// 任务：拷贝html
+// 定义参数
+var app = {  
+    model:'dev',
+    srcPath:'src/',
+    distPath:'dist/'
+}
 
 function copyHtml(){
     // 选取到src目录下的所有html文件 （为了测试效果，请自己再src目录下随便创建两个html文件咯）
-    return src('src/*.html')
+    return src(app.srcPath + '*.html')
         .pipe(dest('dist')); // 将html拷贝到dist目录下，没有dist会自动生成
 }
 // 任务： 图片压缩
 function build_image(){
-    return src('src/images/**/*')
+    return src(app.srcPath + 'images/**/*.*')
         //.pipe(imagemin()) // 调用插件imagemin
-        .pipe(dest('dist/images')) // 压缩后的图片输出目录
+        .pipe(dest(app.distPath + 'images')) // 压缩后的图片输出目录
     
 }
 // sass 编译 src目录下件文件夹sass然后在里面新建一个sass文件，按照sass的语法写一些样式
 function build_sass(){
-    return src('src/sass/*') // 获取sass文件（*.scss）
+    return src(app.srcPath + 'sass/*') // 获取sass文件（*.scss）
         .pipe(sass().on('error',sass.logError)) // 执行sass插件，并检查错误
-        .pipe(dest('dist/css')); // 输出css文件的路径
+        .pipe(dest(app.distPath + 'css')); // 输出css文件的路径
 }
 // 压缩css 
 function build_css(){
-    return src('src/css/*') // 获取css文件（*.css）
+    return src(app.srcPath + 'css/*') // 获取css文件（*.css）
         .pipe(css()) // 执行cleanCSS插件，并检查错误
-        .pipe(dest('dist/css')); // 输出css文件的路径
+        .pipe(dest(app.distPath + 'css')); // 输出css文件的路径
 }
 // 合并压缩代码，src目录下新建文件夹js然后在里面创建几个js文件
  function build_js(){
-    return src('src/js/*.js') // 获取js文件
+    return app.model==='dev' ? src(app.srcPath + 'js/*.js') // 获取js文件
+        .pipe(dest(app.distPath + 'js')) // 输出合并压缩后的文件路径
+        :
+        src(app.srcPath + 'js/*.js') // 获取js文件
         //.pipe(concat('main.js')) // 合并文件为main.js
         .pipe(uglify()) // 压缩js代码
-        .pipe(dest('dist/js')) // 输出合并压缩后的文件路径
+        .pipe(dest(app.distPath + 'js')) // 输出合并压缩后的文件路径
 }
 
 function build_fonts(){
-    return src('src/fonts/**/*.*').pipe(dest('dist/fonts'));
+    return src(app.srcPath + 'fonts/**/*.*').pipe(dest(app.distPath + 'fonts'));
 }
 function build_skin(){
-    return src('src/skin/**/*.*').pipe(dest('dist/skin'));
+    return src(app.srcPath + 'skin/**/*.*').pipe(dest(app.distPath + 'skin'));
 }
 function build_lib(){
-    return src('src/lib/**/*.*').pipe(dest('dist/lib'));
+    return src(app.srcPath + 'lib/**/*.*').pipe(dest(app.distPath + 'lib'));
 }
 function build_data(){
-    return src('src/data/**/*.*').pipe(dest('dist/data'));
+    return src(app.srcPath + 'data/**/*.*').pipe(dest(app.distPath + 'data'));
 }
 function build_template(){
-    return src('src/template/**/*.*').pipe(dest('dist/template'));
+    return src(app.srcPath + 'template/**/*.*').pipe(dest(app.distPath + 'template'));
 }
 // 监听文件是否发生变化
 function build_watch () {
@@ -66,9 +75,11 @@ function build_watch () {
     watch('src/data/*',build_data);
     watch('src/template/*',build_template);
 }
-
+var dev=[copyHtml,build_image, build_js,build_sass,build_css,build_fonts,build_skin,build_lib,build_data,build_template,build_watch]
+var pro=[copyHtml,build_image, build_js,build_sass,build_css,build_fonts,build_skin,build_lib,build_data,build_template,build_watch]
   // 定义默认任务 , 这里的默认任务default不能随便定义 只能是default,后面接受一个数组作为参数，
   //传入默认执行的任务，后面还可以接受一个回调函数用以执行默认任务代码这里就不给做展示了
 //exports.build = build;
-exports.default = series(copyHtml,build_image, build_js,build_sass,build_css,build_fonts,build_skin,build_lib,build_data,build_template,build_watch);
+exports.default = series(dev);
 //gulp.task('default',['message','copyHtml','sass','scripts','imageMin','watch']);
+
--- src/js/example.js
+++ src/js/example.js
@@ -1,27 +1,28 @@
 $(function() {
-    $("#div_about").hide();
-    var account, version;
+    $("#div_about").hide()
+    var account, version
     if (!$.kui.store.enable) {
-        account = $.trim($.kui.util.getUrlParam("account"));
-        version = $.trim($.kui.util.getUrlParam("version"));
+        account = $.trim($.kui.util.getUrlParam("account"))
+        version = $.trim($.kui.util.getUrlParam("version"))
     } else {
-        account = $.trim($.kui.store.get("account"));
-        version = $.trim($.kui.store.get("version"));
+        account = $.trim($.kui.store.get("account"))
+        version = $.trim($.kui.store.get("version"))
     }
 
-    version = !version ? 'iframe' : version;
+    version = !version ? 'iframe' : version
     $.ajax({
         url: './data/top_menu.json?t=' + new Date().getTime(),
         type: 'get',
         success: function(obj) {
             if ("string" == typeof obj && obj.indexOf('html') > 0) {
-                window.location.href = "login.html";
-                return;
+                window.location.href = "login.html"
+                return
             } else {
-                obj=Object.prototype.toString.call(obj) === '[object Object]'?obj:$.parseJSON(obj);
+                obj=Object.prototype.toString.call(obj) === '[object Object]'?obj:$.parseJSON(obj)
                 if (obj.result == 18) {
-                    window.location.href = "login.html";
+                    window.location.href = "login.html"
                 } else {
+                    obj.data[1].active="active"
                     //加载header模块
                     var header_data = {
                         add: '增加',
@@ -32,85 +33,96 @@ $(function() {
                         esc: '退出',
                         logoimg: "./images/logo-while.png",
                         list: obj.data
-                    };
-                    $.kui.kjax.atemplate('header', 'header', './header.html', header_data, '#data');
+                    }
+                   
+                    $.kui.kjax.atemplate('header', 'header', './header.html', header_data, '#data')
                     //获取首页菜单
                     $.ajax({
                         url: './data/menu_' + version + '_' + obj.pid + '.json?t=' + new Date().getTime(), 
                         type: 'get',
                         success: function(obj) {
-                            obj=Object.prototype.toString.call(obj) === '[object Object]'?obj:$.parseJSON(obj);
+                            obj=Object.prototype.toString.call(obj) === '[object Object]'?obj:$.parseJSON(obj)
                             if (obj.result == 0) {
-                                var option = { animate: true, data: obj.data };
-                                $('#kui-tree-menu').tree(option);
+                                var option = { animate: true, data: obj.data }
+                                $('#kui-tree-menu').tree(option)
                                 $('.kui-tree-menu-arrow-a').click(function() {
-                                    var obj = $('.kui-tree-menu-arrow-a');
+                                    var obj = $('.kui-tree-menu-arrow-a')
                                     if ($(obj).hasClass("open")) {
-                                        $(obj).removeClass("open");
-                                        $("body").removeClass("big-page");
+                                        $(obj).removeClass("open")
+                                        $("body").removeClass("big-page")
                                     } else {
-                                        $(obj).addClass("open");
-                                        $("body").addClass("big-page");
+                                        $(obj).addClass("open")
+                                        $("body").addClass("big-page")
                                     }
-                                });
+                                })
                                 // 手动通过点击模拟高亮菜单项
                                 $('#kui-tree-menu').on('click', 'a', function() {
-                                    $('#kui-tree-menu li.active').removeClass('active');
-                                    $(this).closest('li').addClass('active');
-                                });
-                                inittab(account, version);
+                                    $('#kui-tree-menu li.active').removeClass('active')
+                                    $(this).closest('li').addClass('active')
+                                })
+                                inittab(account, version)
                             } else if (obj.result == 18) {
-                                window.location.href = "login.html";
+                                window.location.href = "login.html"
                             }
                         },
                         error: function(jqXHR, textStatus, errorThrown) {
-                            console.log(jqXHR.responseText);
-                            console.log(textStatus);
-                            console.log(errorThrown);
+                            console.log(jqXHR.responseText)
+                            console.log(textStatus)
+                            console.log(errorThrown)
                         }
-                    });
+                    })
                 }
             }
 
         },
         error: function(jqXHR, textStatus, errorThrown) {
-            console.log(jqXHR.responseText);
-            console.log(textStatus);
-            console.log(errorThrown);
+            console.log(jqXHR.responseText)
+            console.log(textStatus)
+            console.log(errorThrown)
         }
-    });
+    })
 
 
 
 
-});
+})
 
 function menu(pid, e) {
-    var version = "";
-    if (!$.kui.store.enable) {
-        version = $.trim($.kui.util.getUrlParam("version"));
-    } else {
-        version = $.trim($.kui.store.get("version"));
+    $('.navbar-left li').removeClass('active')
+    $(e).parent().addClass('active')
+    if(pid==="0"){
+       window.location.href="introduction.html"
     }
-    if (null != pid && pid!=0) {
-        $('.navbar-left li').removeClass('active');
-        $(e).parent().addClass('active');
+    if(pid==="2"){
+        $(".kui-ktabs-open").hide()
+    }
+    if (pid==="1") {
+        var version = null
+        if (!$.kui.store.enable) {
+            version = $.trim($.kui.util.getUrlParam("version"))
+        } else {
+            version = $.trim($.kui.store.get("version"))
+        }
+        version = version ? version : 'iframe'
+        let url='./data/menu_' + version + '_' + pid + '.json?t=' + new Date().getTime()
+        console.log(url)
         $.ajax({
-            url: './data/menu_' + version + '_' + pid + '.json?t=' + new Date().getTime(),
+            url: url,
             success: function(obj) {
                 if (obj.result == 0) {
-                    var menu = $('#kui-tree-menu').data('kui.tree');
-                    menu.reload(obj.data);
+                    var menu = $('#kui-tree-menu').data('kui.tree')
+                    menu.reload(obj.data)
                 } else if (obj.result == 18) {
-                    window.location.href = "login.html";
+                    window.location.href = "login.html"
                 }
+                $(".kui-ktabs-open").show()
             },
             error: function(jqXHR, textStatus, errorThrown) {
-                console.log(jqXHR.responseText);
-                console.log(textStatus);
-                console.log(errorThrown);
+                console.log(jqXHR.responseText)
+                console.log(textStatus)
+                console.log(errorThrown)
             }
-        });
+        })
     }
 }
 
@@ -119,33 +131,35 @@ function inittab(account, version) {
         //初始化pjax
         $('a[data-pjax]').pjax(null, null, {
             changeAddress: true
-        });
+        })
 
         $(document).on('pjax:send', function() {
             //在pjax发送请求时，显示loading动画层
             layer.load(1, {
                 shade: 0.5 //0.1透明度的白色背景
-            });
-        });
+            })
+        })
         $(document).on('pjax:end', function() {
             //在pjax发送请求时，显示loading动画层
-            layer.closeAll();
-        });
+            layer.closeAll()
+        })
         //pjax加载
-        $.kui.pjaxTabs.init();
+        $.kui.pjaxTabs.init()
     } else {
-        var ktab = ' <a title="首页" href="home.html" target="iframe-0"><span>首页</span></a>';
-        $("#ktab").html(ktab);
+        var ktab = ' <a title="首页" href="home.html" target="iframe-0"><span>首页</span></a>'
+        $("#ktab").html(ktab)
 
-        var kpage = '<iframe name="iframe-0" class="page-frame animation-fade active" src="home.html" frameborder="0"></iframe>';
-        $(".kui-main").html(kpage);
+        var kpage = '<iframe name="iframe-0" class="page-frame animation-fade active" src="home.html" frameborder="0"></iframe>'
+        $(".kui-main").html(kpage)
 
-        $.kui.iframeTabs.init();
+        $.kui.iframeTabs.init()
         $('body').css({
             "overflow-x": "hidden",
             　　"overflow-y": "hidden"
-        });
+        })
     }
+
+   
 }
 
 function about() {
@@ -154,18 +168,18 @@ function about() {
         skin: 'layui-layer-rim', //加上边框
         area: ['820px', '340px'], //宽高
         content: $("#div_about").html()
-    });
+    })
 }
 
 function skin(e) {
-    console.log($(e).attr('data-val'));
-    var dv = $(e).attr('data-val');
+    console.log($(e).attr('data-val'))
+    var dv = $(e).attr('data-val')
     if (dv) {
-        var head = document.getElementsByTagName('head')[0];
-        var link = document.createElement('link');
-        link.href = 'skin/' + dv + '/skin.css';
-        link.rel = 'stylesheet';
-        link.type = 'text/css';
-        head.appendChild(link);
+        var head = document.getElementsByTagName('head')[0]
+        var link = document.createElement('link')
+        link.href = 'skin/' + dv + '/skin.css'
+        link.rel = 'stylesheet'
+        link.type = 'text/css'
+        head.appendChild(link)
     }
 }
